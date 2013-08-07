5c4704ec6161d04a435ae6c52b4311757774f858
Eric <ericwangqing@gmail.com>
2013-08-07 13:05:42 +0800

    start run dev

---
+++ assets/App/shell.js
@@ -0,0 +1,20 @@
+﻿define(['durandal/plugins/router', 'durandal/app'], function (router, app) {
+
+    return {
+        router: router,
+        search: function() {
+            //It's really easy to show a message box.
+            //You can add custom options too. Also, it returns a promise for the user's response.
+            app.showMessage('Search not yet implemented...');
+        },
+        activate: function () {
+            // router.mapNav('welcome');
+            // router.mapNav('flickr');
+            router.map([
+                {url: 'welcome', moduleId: 'welcome', name: '欢迎'},
+                {url: 'flickr', moduleId: 'flickr', name: '相册'}
+            ]);
+            return router.activate('welcome');
+        }
+    };
+});
